0b7c8133e2b4a70eae990fe8bb4943c146953bc5
Roosevelt Burden <rooseveltburden@gmail.com>
2023-05-16 06:39:32 -0600

    added seed data for businesses table

--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
-from app.models.db import db, environment, SCHEMA
+from app.models.db import environment
+from .businesses import seed_businesses, undo_businesses
 from .locations import seed_locations, undo_locations
 from .user_locations import seed_user_locations, undo_user_locations
 from .users import seed_users, undo_users
@@ -17,17 +18,20 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_businesses()
         undo_user_locations()
         undo_locations()
         undo_users()
     seed_users()
     seed_locations()
     seed_user_locations()
+    seed_businesses()
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_businesses()
     undo_user_locations()
     undo_locations()
     undo_users()
---
+++ backend/app/seeds/businesses.py
@@ -0,0 +1,43 @@
+import json
+import random
+from app.models import db, Business, environment, SCHEMA
+from sqlalchemy.sql import text
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_businesses():
+    data = open('app/seeds/data/businesses.json')
+    businesses = json.load(data)
+
+    print("\nSeeding businesses table...")
+    loc_arr = list(range(1, 51))
+    for business in businesses:
+        location = random.choice(loc_arr)
+        loc_arr.remove(location)
+
+        new_user = Business(
+            name=business['name'],
+            description=business['description'],
+            category=business['category'],
+            location_id=location,
+        )
+        db.session.add(new_user)
+
+    db.session.commit()
+    print("Businesses table seeded.")
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_businesses():
+    if environment == "production":
+        db.session.execute(
+            f"TRUNCATE table {SCHEMA}.businesses RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM businesses"))
+
+    db.session.commit()
