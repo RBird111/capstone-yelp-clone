b8cd65552ec98ca0e9cf1f4105709cd2728cb9f8
Roosevelt Burden <rooseveltburden@gmail.com>
2023-05-16 03:38:33 -0600

    created migrations for new tables

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,7 @@
-from .db import db
+from .business import Business
+from .db import db, environment, SCHEMA
+from .image import Image
+from .location import Location
+from .review import Review
+from .user_location import UserLocation
 from .user import User
-from .db import environment, SCHEMA
