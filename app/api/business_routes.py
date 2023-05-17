98e5d6d086135e4eb6838c835aded002912b070c
Roosevelt Burden <rooseveltburden@gmail.com>
2023-05-17 15:29:16 -0600

    added business form

---
+++ app/api/business_routes.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+business_routes = Blueprint("businesses", __name__)
---
+++ app/forms/business.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, ValidationError
+
+
+class BusinessForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    description = StringField('description', validators=[DataRequired()])
+    category = StringField('category', validators=[DataRequired()])
+    location_id = StringField('location_id', validators=[DataRequired()])
