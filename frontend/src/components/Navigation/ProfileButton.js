9829772c49c840e9e3e1d79b827ddece0bae3974
Roosevelt Burden <rooseveltburden@gmail.com>
2023-05-17 13:59:19 -0600

    added a little styling to landing page

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -42,14 +42,12 @@ function ProfileButton({ user }) {
       <button onClick={openMenu}>
         <i className="fas fa-user-circle" />
       </button>
-      <ul className={ulClassName} ref={ulRef}>
+      <div className={ulClassName} ref={ulRef}>
         {user ? (
           <>
-            <li>{user.username}</li>
-            <li>{user.email}</li>
-            <li>
-              <button onClick={handleLogout}>Log Out</button>
-            </li>
+            <p>{user.username}</p>
+            <p>{user.email}</p>
+            <button onClick={handleLogout}>Log Out</button>
           </>
         ) : (
           <>
@@ -66,7 +64,7 @@ function ProfileButton({ user }) {
             />
           </>
         )}
-      </ul>
+      </div>
     </div>
   );
 }
