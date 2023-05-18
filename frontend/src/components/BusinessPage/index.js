19ee78dce00bb84e7901aa8af8a4a70a6c276671
Roosevelt Burden <rooseveltburden@gmail.com>
2023-05-18 09:55:38 -0600

    added really rough business page

---
+++ frontend/src/components/BusinessPage/index.js
@@ -0,0 +1,53 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+
+import "./BusinessPage.scss";
+import { getBusiness } from "../../store/business";
+
+const BusinessPage = () => {
+  const dispatch = useDispatch();
+
+  const { businessId } = useParams();
+  const business = useSelector((state) => state.business.currBusiness);
+  const { name, description, category, location, reviews, images } = business;
+
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(getBusiness(businessId)).then(() => setIsLoaded(true));
+  }, [businessId, dispatch]);
+
+  if (!isLoaded) return <>Loading...</>;
+
+  return (
+    <div className="business-page">
+      <h1>Name: {name}</h1>
+      <h2>Category: {category}</h2>
+
+      <div className="location">
+        <p>Address: {location.address}</p>
+        <p>City: {location.city}</p>
+        <p>State: {location.state}</p>
+      </div>
+
+      <p>Description: {description}</p>
+
+      <h3>Reviews:</h3>
+      {Object.values(reviews).map((review) => (
+        <div key={review.id}>
+          <p>Rating: {review.rating}</p>
+
+          <p>Body: {review.body}</p>
+        </div>
+      ))}
+
+      <h3>Images:</h3>
+      {Object.values(images).map((image) => (
+        <img key={image.id} src={image.url} alt="business" />
+      ))}
+    </div>
+  );
+};
+
+export default BusinessPage;
