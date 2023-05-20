86c955a8e0c970653238fcd045a354623fd32603
Roosevelt Burden <rooseveltburden@gmail.com>
2023-05-20 13:13:46 -0600

    landing page styling

--- frontend/src/components/FormElements/ProfileIcon/index.js
+++ frontend/src/components/FormElements/ProfileIcon/index.js
@@ -3,7 +3,7 @@ import "./ProfileIcon.scss";
 const ProfileIcon = ({ height = "40px", width = "40px" }) => {
   return (
     <div style={{ height: height, width: width }} className="icon-div">
-      <i class="fa-solid fa-user fa-lg" />
+      <i className="fa-solid fa-user fa-lg" />
     </div>
   );
 };
