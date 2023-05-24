b75a49cc87119915638c73ed8b6ca078f84bda62
Roosevelt Burden <rooseveltburden@gmail.com>
2023-05-24 16:08:41 -0600

    more styling adjustments

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -16,10 +16,10 @@ export const normalize = (data) => {
   // Check to see if data is an object
   switch (typeof data) {
     case "object": {
-      // If null return
+      // If object is null return
       if (data === null) return null;
 
-      // If so check to see if data is an array
+      // Check to see if data is an array
       switch (Array.isArray(data)) {
         // If it's an array then normalize each item
         case true:
@@ -29,7 +29,7 @@ export const normalize = (data) => {
             return acc;
           }, {});
 
-        // If not then its a POJO
+        // Otherwise its a POJO
         default:
           // So we normalize the entries of the POJO
           return Object.entries(data).reduce((acc, [k, v]) => {
@@ -39,8 +39,10 @@ export const normalize = (data) => {
           }, {});
       }
     }
+    // If data is undefined
     case "undefined":
       return null;
+
     // If it's not an object then just return it to the caller
     default:
       return data;
