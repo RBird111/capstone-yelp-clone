af533f0e7c0ddbfd229eab3eed98093cee3c63e5
Roosevelt Burden <rooseveltburden@gmail.com>
2023-05-27 13:00:13 -0600

    added images to review card and added titles to delete icons

--- frontend/src/components/ReviewCard/index.js
+++ frontend/src/components/ReviewCard/index.js
@@ -1,56 +1,72 @@
-import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import { NavLink, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 import "./ReviewCard.scss";
 import StarRatingBar from "../FormElements/StarRatingBar";
 import ProfileIcon from "../FormElements/ProfileIcon";
 
 const ReviewCard = ({ review }) => {
+  const history = useHistory();
+
+  const imageUrl = Object.values(review.images)[0].url_regular;
+
   return (
     <div className="review-card">
-      <div className="user-info">
-        <ProfileIcon />
+      <div
+        className="background"
+        style={{
+          // backgroundImage: `linear-gradient(white 8vw, transparent 8vw 18vw, white 18vw), url(${imageUrl})`,
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
 
-        <div className="p-div">
-          <p className="name">
-            {review.user.first_name} {review.user.last_name[0]}.
-          </p>
+      <div className="wrap">
+        <div className="user-info">
+          <ProfileIcon />
 
-          <p className="wrote">Wrote a review</p>
+          <div className="p-div">
+            <p className="name">
+              {review.user.first_name} {review.user.last_name[0]}.
+            </p>
+
+            <p className="wrote">Wrote a review</p>
+          </div>
+        </div>
+
+        <div className="star-rating-bar">
+          <NavLink to={`/business/${review.business.id}`}>
+            {review.business.name}
+          </NavLink>
+
+          <StarRatingBar rating={review.rating} />
         </div>
       </div>
 
-      <div className="star-rating-bar">
-        <NavLink to={`/business/${review.business.id}`}>
-          {review.business.name}
-        </NavLink>
+      <div className="r-wrap">
+        <div className="review-body">
+          <p className="body">{review.body}</p>
 
-        <StarRatingBar rating={review.rating} />
-      </div>
+          <p
+            className="continue"
+            onClick={() => history.push(`/business/${review.business.id}`)}
+          >
+            Continue reading
+          </p>
+        </div>
 
-      <div className="review-body">
-        <p className="body">{review.body}</p>
-
-        <p
-          className="continue"
-          onClick={() => alert("TODO: Redirect to Review Page")}
-        >
-          Continue reading
-        </p>
-      </div>
-
-      <div className="icon-bar">
-        {[
-          "fa-regular fa-lightbulb fa-xl",
-          "fa-regular fa-face-laugh fa-xl",
-          "fa-regular fa-face-sad-cry fa-xl",
-        ].map((className, idx) => (
-          <div className="icon" key={idx}>
-            <i
-              onClick={() => alert("Feature not yet implemented")}
-              className={className}
-            />
-          </div>
-        ))}
+        <div className="icon-bar">
+          {[
+            "fa-regular fa-lightbulb fa-xl",
+            "fa-regular fa-face-laugh fa-xl",
+            "fa-regular fa-face-sad-cry fa-xl",
+          ].map((className, idx) => (
+            <div className="icon" key={idx}>
+              <i
+                onClick={() => alert("Feature not yet implemented")}
+                className={className}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
