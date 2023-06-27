d09e02be63b1ebc5f85baef39b4c2346cd778cad
Roosevelt Burden <rooseveltburden@gmail.com>
2023-06-27 14:21:12 -0600

    added gallery to user profile page and ability to delete images

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -105,7 +105,7 @@ def delete_image(id):
     if not image:
         return {'errors': 'Image could not be found'}
 
-    ret = remove_file_from_s3(image["url"])
+    ret = remove_file_from_s3(image.url)
 
     if ret != True:
         return ret
